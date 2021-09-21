0a1fc5aed99c886b27c596a5e56057580b1f39a0
MeganSorenson <85324120+MeganSorenson@users.noreply.github.com>
2021-09-21 15:33:00 -0600

    remember version 5

--- lecture/remembergame/Remember-Version5Code.py
+++ lecture/remembergame/Remember-Version5Code.py
@@ -4,6 +4,7 @@
 # Version 2 Code - clear, conditional feedback, identify and replace adjacent duplicate line grups with a for statement
 # Version 3 Code - software quality requirement; identify any literals that occur more than once and bind to an identifer and replace literal with identifier, random selection of 4 words, randome selection of answer
 # Version 4 Code - player can play the game multiple times
+# Version 5 Code - identify non-adjacent duplicate line groups, define a function, place the linw group in the body of the function, replace all other occurences of line group with user defined function
 
 # https://docs.python.org/3/library/time.htmlhttps://docs.python.org/3/library/time.html
 # https://docs.python.org/3/library/os.html
@@ -12,22 +13,26 @@ import time
 import os
 import random
 
-continue_game = True
-while continue_game:
+
+def display_header():
+    # clears screen and displays header
+    # returns None
     # clear screen
     clear_command = "clear"  # clear command for mac
     if os.name == 'nt':
         clear_command = 'cls'  # clear command for windows
     os.system(clear_command)
-
-    # display header
+    # print header
     header_border = "-" * 80
     header_content = "Remember The Word"
     print(header_border)
     print(header_content)
     print(header_border)
 
-    # display instructions
+
+def display_instructions():
+    # displays instructions
+    # returns None
     filename = "instructions.txt"
     filemode = "r"
     file = open(filename, filemode)
@@ -35,20 +40,10 @@ while continue_game:
     file.close()
     print(instructions)
 
-    # prompt player to press enter
-    input("Press enter key to display the words.")
 
-    # clear screens
-    os.system(clear_command)
-    print(header_border)
-    print(header_content)
-    print(header_border)
-
-    # display 4 words
-    #   displayed one at a time
-    #   there is a 1 second pause before word disapears and next word appears
-    #   words are differetn each time game is played
-    #   words are chosen randomly from a list
+def choose_words():
+    # samples 4 words randomly from a file
+    # returns list
     filename = "words.txt"
     filemode = "r"
     file = open(filename, filemode)
@@ -56,23 +51,42 @@ while continue_game:
     file.close()
     words = all_words.splitlines()
     four_words = random.sample(words, 4)
+    return four_words
 
+
+def display_words(four_words):
+    # displays the four words
+    # four_words is parameter type list
+    # returns None
     pause_time = 1
-
-    answer = random.choice(four_words)
-    first_letter = answer[0]
-
     for word in four_words:
         print(word)
         time.sleep(pause_time)
-        os.system(clear_command)
-        # display header
-        print(header_border)
-        print(header_content)
-        print(header_border)
+        display_header()
+
+
+# main program starts here
+continue_game = True
+while continue_game:
+    display_header()
+    display_instructions()
+
+    # prompt player to press enter
+    input("Press enter key to display the words.")
+    display_header()
+
+    # display 4 words
+    #   displayed one at a time
+    #   there is a 1 second pause before word disapears and next word appears
+    #   words are different each time game is played
+    #   words are chosen randomly from a list
+    four_words = choose_words()
+    answer = random.choice(four_words)
+    first_letter = answer[0]
+    display_words(four_words)
 
     # prompt player to enter word thats starts with letter
-    #   answer is chosenn randomly from 4 displayed words
+    #   answer is chosen randomly from 4 displayed words
     #   prompt is formulated using the first letter of the answer
     guess = input("What word starts with the letter " + first_letter + "?")
 
