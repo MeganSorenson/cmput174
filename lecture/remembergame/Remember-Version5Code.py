799e30a1c09f2ebf8c6ad5b5bc03768eeb8106cf
MeganSorenson <85324120+MeganSorenson@users.noreply.github.com>
2021-09-23 14:42:18 -0600

    remember version5 part 2

--- lecture/remembergame/Remember-Version5Code.py
+++ lecture/remembergame/Remember-Version5Code.py
@@ -65,43 +65,65 @@ def display_words(four_words):
         display_header()
 
 
-# main program starts here
-continue_game = True
-while continue_game:
-    display_header()
-    display_instructions()
-
-    # prompt player to press enter
-    input("Press enter key to display the words.")
-    display_header()
-
-    # display 4 words
-    #   displayed one at a time
-    #   there is a 1 second pause before word disapears and next word appears
-    #   words are different each time game is played
-    #   words are chosen randomly from a list
-    four_words = choose_words()
-    answer = random.choice(four_words)
-    first_letter = answer[0]
-    display_words(four_words)
-
+def prompt_guess(first_letter):
     # prompt player to enter word thats starts with letter
-    #   answer is chosen randomly from 4 displayed words
-    #   prompt is formulated using the first letter of the answer
+    # returns string
     guess = input("What word starts with the letter " + first_letter + "?")
+    return guess
 
-    # evaluate the answer and display feedback
+
+def display_result(guess, answer):
+    # evaluates result and displays the result
+    # returns None
     if guess.lower() == answer.lower():
         # congratulations if correct
         print("Congratulations, you are correct.")
     else:
         # condolence if wrong
         print("Sorry you entered " + guess + ".")
-
     print("The answer was " + answer + ".")
 
-    # prompt player to play again
-    #   restarts if player chooses so
-    #   or program terminates
+
+def prompt_continue():
+    # prompts the player to continue or not
+    # returns bool
     reply = input("Play again? y/N").lower()
     continue_game = reply == "y"
+    return continue_game
+
+
+def main():
+    continue_game = True
+    while continue_game:
+        display_header()
+        display_instructions()
+
+        # prompt player to press enter
+        input("Press enter key to display the words.")
+        display_header()
+
+        # display 4 words
+        #   displayed one at a time
+        #   there is a 1 second pause before word disapears and next word appears
+        #   words are different each time game is played
+        #   words are chosen randomly from a list
+        four_words = choose_words()
+        answer = random.choice(four_words)
+        first_letter = answer[0]
+        display_words(four_words)
+
+        # prompt player to enter word thats starts with letter
+        #   answer is chosen randomly from 4 displayed words
+        #   prompt is formulated using the first letter of the answer
+        guess = prompt_guess(first_letter)
+
+        # evaluate the answer and display feedback
+        display_result(guess, answer)
+
+        # prompt player to play again
+        #   restarts if player chooses so
+        #   or program terminates
+        continue_game = prompt_continue()
+
+
+main()
